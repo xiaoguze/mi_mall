61cb9f90ba8cc15ade9d6a6f2926b64354fd3eba
xiaoze <2393015415@qq.com>
2020-10-11 11:14:17 +0800

    插件安装 vue-lazyload vue-axios element-ui swiper vue-cookie

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,15 @@ module.exports = {
     devServer:{
         host:"localhsot",
         port:8080,
-        proxy:{
-            '/api':{
+        // proxy:{
+        //     '/api':{
                 
-            }
-        }
+        //     }
+        // }
     },
     configureWebpack:{
         //文件别命
-        reslove:{
+        resolve:{
             alias:{
                 "components":"@/components",// 组件别命
                 "views":"@/views", // 页面别命
